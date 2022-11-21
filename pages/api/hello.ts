e23c0f30e54c711136d1320bcfa7f5b133bfaed9
Samuel <samuel@plasmic.app>
2022-11-21 12:14:12 -0300

    remove api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -13,13 +13,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const body = JSON.parse(req.body);
-  // if (body.endpoint === "match") {
-  //   res.status(200).json(match);
-  //   return ;
-  // } else if (body.endpoint === "standings") {
-  //   res.status(200).json(standings);
-  //   return ;
-  // }
+  if (body.endpoint === "match") {
+    res.status(200).json(match);
+    return ;
+  } else if (body.endpoint === "standings") {
+    res.status(200).json(standings);
+    return ;
+  }
   const data = await (await fetch("http://api.cup2022.ir/api/v1/" + body.endpoint, {
     headers: {
       Authorization: `Bearer ${process.env.WC_API_TOKEN}`
